e3f9fd2d90b28cfb60a14e8e2ca1e6d2dc15d198
Kostya <hegona3@gmail.com>
2026-04-30 18:28:58 +0100

    feat: SwiftPM scaffold with empty AppDelegate

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "Corder",
+    platforms: [.macOS(.v13)],
+    products: [
+        .executable(name: "Corder", targets: ["Corder"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0"),
+        .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "Corder",
+            dependencies: [
+                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "Swifter", package: "swifter")
+            ],
+            resources: [
+                .copy("Resources/web")
+            ]
+        ),
+        .testTarget(
+            name: "CorderTests",
+            dependencies: ["Corder"]
+        )
+    ]
+)
